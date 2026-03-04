0ba73ac67c5f13201ef9e420a1b6f14a72a8f38d
Zeteox <zeteox01@gmail.com>
2026-03-04 20:21:53 +0100

    feat: made the achievement mecanics

--- public/salvageGame.php
+++ public/salvageGame.php
@@ -1,6 +1,7 @@
 <?php
-require('../app/database/Database.php');
-require_once('../app/utils/EnvUtils.php');
+require __DIR__ . '/../app/database/Database.php';
+require_once __DIR__ . '/../app/utils/EnvUtils.php';
+require_once __DIR__ . '/../app/utils/AchievementsUtils.php';
 session_start();
 
 $user_id = $_SESSION['user_id'] ?? null;
@@ -16,5 +17,9 @@ if ($gameId === null) {
 loadEnv();
 Database::connect("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME') . ";charset=utf8", getenv('DB_USER'), getenv('DB_PASS'));
 Database::deleteGameFromUser($_SESSION['user_id'], $gameId);
+
+updateAchievements($user_id);
+
+Database::disconnect();
 header('Location: library.php');
 ?>
